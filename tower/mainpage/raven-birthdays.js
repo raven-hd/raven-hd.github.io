f81bbdbc047b34261c85dd5af25113e34cad2055
blake-el <ellington.ravenclaw@gmail.com>
2026-09-08 17:34:38 +0300

    Add files via upload

---
+++ tower/mainpage/raven-birthdays.js
@@ -0,0 +1,462 @@
+window.RAVEN_BIRTHDAYS = [
+  {
+    "name": "Лиария Спраут",
+    "month": 1,
+    "day": 6
+  },
+  {
+    "name": "Мансур Селестин",
+    "month": 1,
+    "day": 7
+  },
+  {
+    "name": "Патриция Роксинбург",
+    "month": 1,
+    "day": 10
+  },
+  {
+    "name": "Эвелин Мур",
+    "month": 1,
+    "day": 13
+  },
+  {
+    "name": "Сорен Вейл",
+    "month": 1,
+    "day": 14
+  },
+  {
+    "name": "Винсент Бернкастель",
+    "month": 1,
+    "day": 18
+  },
+  {
+    "name": "Merida Croved",
+    "month": 1,
+    "day": 23
+  },
+  {
+    "name": "Morgane Dave Black",
+    "month": 1,
+    "day": 25
+  },
+  {
+    "name": "Меан Эрбрами",
+    "month": 1,
+    "day": 26
+  },
+  {
+    "name": "Энни Сирин",
+    "month": 1,
+    "day": 28
+  },
+  {
+    "name": "Джейн Батори",
+    "month": 1,
+    "day": 29
+  },
+  {
+    "name": "Рикине Беннетт",
+    "month": 1,
+    "day": 31
+  },
+  {
+    "name": "Alleria Wind",
+    "month": 2,
+    "day": 1
+  },
+  {
+    "name": "Эйлин Крэйн",
+    "month": 2,
+    "day": 2
+  },
+  {
+    "name": "Amber Lawrence",
+    "month": 2,
+    "day": 3
+  },
+  {
+    "name": "Блейк Эллингтон",
+    "month": 2,
+    "day": 9
+  },
+  {
+    "name": "Азали Миллер",
+    "month": 2,
+    "day": 13
+  },
+  {
+    "name": "Хлоя Ларсен",
+    "month": 2,
+    "day": 14
+  },
+  {
+    "name": "Elfie Spellman",
+    "month": 2,
+    "day": 17
+  },
+  {
+    "name": "Fevralina Puf",
+    "month": 2,
+    "day": 19
+  },
+  {
+    "name": "Беата ЛаВей",
+    "month": 2,
+    "day": 23
+  },
+  {
+    "name": "Bessy Abyss",
+    "month": 2,
+    "day": 29
+  },
+  {
+    "name": "Alain Saint-Clair",
+    "month": 3,
+    "day": 1
+  },
+  {
+    "name": "Либретта Виземал",
+    "month": 3,
+    "day": 1
+  },
+  {
+    "name": "Соланж Деллингхейм",
+    "month": 3,
+    "day": 5
+  },
+  {
+    "name": "Adeline Delhi",
+    "month": 3,
+    "day": 7
+  },
+  {
+    "name": "Шарлина Квик",
+    "month": 3,
+    "day": 13
+  },
+  {
+    "name": "Беатрис Хейзел Грэм",
+    "month": 3,
+    "day": 20
+  },
+  {
+    "name": "Эйриан Гамильтон",
+    "month": 3,
+    "day": 26
+  },
+  {
+    "name": "Мирием Лав Морган",
+    "month": 3,
+    "day": 30
+  },
+  {
+    "name": "Джейн Льюис",
+    "month": 3,
+    "day": 31
+  },
+  {
+    "name": "Кэмерин Ствайн",
+    "month": 3,
+    "day": 31
+  },
+  {
+    "name": "Джордж Версетти",
+    "month": 4,
+    "day": 2
+  },
+  {
+    "name": "Кристиан Паренто",
+    "month": 4,
+    "day": 15
+  },
+  {
+    "name": "Мишель Джонс",
+    "month": 4,
+    "day": 17
+  },
+  {
+    "name": "Vladiona",
+    "month": 4,
+    "day": 19
+  },
+  {
+    "name": "Доминик Сен-Клер",
+    "month": 4,
+    "day": 26
+  },
+  {
+    "name": "Ясмина Эстель Блэк",
+    "month": 5,
+    "day": 4
+  },
+  {
+    "name": "Рейчел Лефевр",
+    "month": 5,
+    "day": 15
+  },
+  {
+    "name": "Эмма Джонсон",
+    "month": 5,
+    "day": 18
+  },
+  {
+    "name": "Аделин Иса",
+    "month": 5,
+    "day": 26
+  },
+  {
+    "name": "Миа Одли",
+    "month": 5,
+    "day": 27
+  },
+  {
+    "name": "Альфред Гриндарк",
+    "month": 5,
+    "day": 29
+  },
+  {
+    "name": "Чак Моррисон",
+    "month": 5,
+    "day": 31
+  },
+  {
+    "name": "Лориан Дейл",
+    "month": 6,
+    "day": 19
+  },
+  {
+    "name": "Лана Морская",
+    "month": 6,
+    "day": 20
+  },
+  {
+    "name": "Дейнерис Блэквуд",
+    "month": 6,
+    "day": 23
+  },
+  {
+    "name": "Камилка Копилка",
+    "month": 7,
+    "day": 1
+  },
+  {
+    "name": "Диана Блэквуд",
+    "month": 7,
+    "day": 2
+  },
+  {
+    "name": "Илья Гельвидес",
+    "month": 7,
+    "day": 3
+  },
+  {
+    "name": "Джессика Фелпс",
+    "month": 7,
+    "day": 4
+  },
+  {
+    "name": "Darcy Fist",
+    "month": 7,
+    "day": 7
+  },
+  {
+    "name": "Мерида Морграфт",
+    "month": 7,
+    "day": 11
+  },
+  {
+    "name": "Хелен Фелтон",
+    "month": 7,
+    "day": 15
+  },
+  {
+    "name": "Biefa de Less",
+    "month": 7,
+    "day": 16
+  },
+  {
+    "name": "Isabelle Ebrahimi-Green",
+    "month": 7,
+    "day": 25
+  },
+  {
+    "name": "Изабелла Тёрнер",
+    "month": 7,
+    "day": 27
+  },
+  {
+    "name": "Melanie de Less",
+    "month": 7,
+    "day": 31
+  },
+  {
+    "name": "Эмберли Шрив",
+    "month": 8,
+    "day": 5
+  },
+  {
+    "name": "Lucretia Nox",
+    "month": 8,
+    "day": 8
+  },
+  {
+    "name": "Дэгни Таггарт",
+    "month": 8,
+    "day": 11
+  },
+  {
+    "name": "Винсент Бернкастель",
+    "month": 8,
+    "day": 16
+  },
+  {
+    "name": "Джон МакЭйб",
+    "month": 8,
+    "day": 26
+  },
+  {
+    "name": "Элиор Лонс",
+    "month": 8,
+    "day": 30
+  },
+  {
+    "name": "Милисента О'Лири",
+    "month": 9,
+    "day": 4
+  },
+  {
+    "name": "Trinity Jenkin",
+    "month": 9,
+    "day": 5
+  },
+  {
+    "name": "Джен Леминот",
+    "month": 9,
+    "day": 22
+  },
+  {
+    "name": "Мари Франсуа",
+    "month": 9,
+    "day": 23
+  },
+  {
+    "name": "Криста",
+    "month": 9,
+    "day": 25
+  },
+  {
+    "name": "Basil Croved",
+    "month": 10,
+    "day": 1
+  },
+  {
+    "name": "Эйш Николеску",
+    "month": 10,
+    "day": 10
+  },
+  {
+    "name": "Мерелана Ханрахан",
+    "month": 10,
+    "day": 12
+  },
+  {
+    "name": "Аделия Бекер",
+    "month": 10,
+    "day": 13
+  },
+  {
+    "name": "Эвия Уайт",
+    "month": 10,
+    "day": 13
+  },
+  {
+    "name": "Артемий Фроус",
+    "month": 10,
+    "day": 14
+  },
+  {
+    "name": "Frida Stray",
+    "month": 10,
+    "day": 16
+  },
+  {
+    "name": "Септима Аллен",
+    "month": 10,
+    "day": 16
+  },
+  {
+    "name": "Корделия Саулен",
+    "month": 10,
+    "day": 18
+  },
+  {
+    "name": "Джулиан Вернер",
+    "month": 10,
+    "day": 31
+  },
+  {
+    "name": "Рената Алтейд",
+    "month": 11,
+    "day": 3
+  },
+  {
+    "name": "Джиллиан Фрай",
+    "month": 11,
+    "day": 9
+  },
+  {
+    "name": "Меган Фрост",
+    "month": 11,
+    "day": 10
+  },
+  {
+    "name": "Гай Сульпициус",
+    "month": 11,
+    "day": 17
+  },
+  {
+    "name": "Айфери Крис",
+    "month": 11,
+    "day": 27
+  },
+  {
+    "name": "Мизуки Хироми",
+    "month": 12,
+    "day": 2
+  },
+  {
+    "name": "Чери Браун",
+    "month": 12,
+    "day": 3
+  },
+  {
+    "name": "Мария Торнаги",
+    "month": 12,
+    "day": 8
+  },
+  {
+    "name": "Уитни Джеральдин",
+    "month": 12,
+    "day": 8
+  },
+  {
+    "name": "Лина Скотт",
+    "month": 12,
+    "day": 19
+  },
+  {
+    "name": "Адрианна Сноу",
+    "month": 12,
+    "day": 25
+  },
+  {
+    "name": "Софи Мид",
+    "month": 12,
+    "day": 27
+  },
+  {
+    "name": "Лилиана Портер",
+    "month": 12,
+    "day": 28
+  }
+];
